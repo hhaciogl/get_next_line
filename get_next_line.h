711e52e6bebd8ec258ba16f92accd90aa8e673db
Hasan Hacıoğlu <hhaciogl@student.42istanbul.com.tr>
2024-11-30 19:15:34 +0300

    init

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 18:57:55 by hhaciogl          #+#    #+#             */
+/*   Updated: 2024/11/30 19:14:58 by hhaciogl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+    #define BUFFER_SIZE 10
+#endif
+char    *get_next_line(int fd);
+
+#endif
