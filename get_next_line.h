2e56e4c744018e039ec36a092e2444b747917105
Hasan Hacıoğlu <hhaciogl@student.42istanbul.com.tr>
2024-11-30 19:25:30 +0300

    norm

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,17 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 18:57:55 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/11/30 19:14:58 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/11/30 19:25:09 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-    #define BUFFER_SIZE 10
-#endif
-char    *get_next_line(int fd);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
 
 #endif
