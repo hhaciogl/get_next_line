2bb32f8194a22eea036848a0681cc4b1d6d284e4
hhaciogl <hhaciogl@student.42istanbul.com.tr>
2024-12-25 01:12:15 +0300

    test: add return null when there is an error

--- zz_1.c
+++ zz_1.c
@@ -1,6 +1,19 @@
 void ok(int expr, char *msg);
+int is(char *s1, char *s2);
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stddef.h>
 
 void ok_1()
 {
-	ok(1, "return null when there is an error");
+	int fd;
+	char *expect;
+	char *actual;
+	
+	fd = open("data1.txt", O_RDONLY);
+	fd = 56;
+	expect = NULL;
+	actual = get_next_line(fd);
+
+	ok(is(actual, expect), "return null when there is an error");
 }
--- zz_2.c
+++
@@ -1,6 +0,0 @@
-void ok(int expr, char *msg);
-
-void ok_2()
-{
-	ok(1, "return null when there is no more line");
-}
