dabee7024580db91e3c6ebcdc141df550df615ea
hhaciogl <hhaciogl@student.42istanbul.com.tr>
2024-12-25 19:30:20 +0300

    feat: fail

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,17 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+#include <stdio.h>
+
 char *get_next_line(int fd)
 {
 	(void)fd;
-	return ((void*)0);
+	char *buf;
+	printf("%i", BUFFER_SIZE);
+	buf = malloc(BUFFER_SIZE + 1);
+	if (NULL == buf)
+	{
+		return (NULL);
+	}
+	return (buf);
 }
