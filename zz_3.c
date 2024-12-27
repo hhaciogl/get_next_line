91cf2daaba37e68f3dade4cf54b05aea9d18b26d
hhaciogl <hhaciogl@student.42istanbul.com.tr>
2024-12-27 08:58:31 +0300

    test: fix ok_3 cmp value

--- zz_3.c
+++ zz_3.c
@@ -2,18 +2,17 @@ void ok(int expr, char *msg);
 int is(char *s1, char *s2);
 #include <fcntl.h>
 #include <string.h>
-#define BUFFER_SIZE 4
 #include "get_next_line.h"
 
 void ok_3()
 {
     int fd;
-    char exp[4];
+    char exp[9];
     char *act;
 
     fd = open("data2.txt", O_RDONLY);
     act = get_next_line(fd);
-    strcpy(exp, "123");
+    strcpy(exp, "1234567\n\0");
     
     ok(is(act, exp), "return new line when lines and buffer same size");
 }
